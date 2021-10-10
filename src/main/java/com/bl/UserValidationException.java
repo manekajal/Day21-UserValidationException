e26ba0cfaf00ed32d5270dc17b409c3d27984a4a
manekajal <manekajal2014@gmail.com>
2021-10-10 10:23:45 +0530

    [Kajal]:User validation Exception

---
+++ src/main/java/com/bl/UserValidationException.java
@@ -0,0 +1,13 @@
+package com.bl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class UserValidationException {
+    public static String firstName(String firstName) {
+        System.out.println("User validation Exception ");
+        return firstName;
+    }
+
+}
---
+++ src/test/java/com/bl/UserValidationExceptionTest.java
@@ -0,0 +1,13 @@
+package com.bl;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class UserValidationExceptionTest {
+
+    @Test
+    public void firstName(){
+        System.out.println();
+    }
+
+}
